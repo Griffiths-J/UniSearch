3cc0aff0762559aa927f0f9cc4fe323ae118ec04
griffiths <you@example.com>
2026-03-12 12:12:30 +0000

    i've written the code for receiving the inputs into an object and also error handling

--- script/take-values.js
+++ script/take-values.js
@@ -8,18 +8,35 @@ async function unis(){
  const UMAT = data[3].UMAT;
 
 
-function values(){  
+function Getvalues(){
+
+        const university = document.querySelector(".getsch-select-value").value;
+        if(university==="Select University" || university ===""){
+          document.querySelector(".prompt").innerHTML="Select university and all inputs"
+          throw new Error("Select university");
+        }
+
          const userResult=[];
         
          //core Suvjects
         const allCRows= document.querySelectorAll('.core-instance'); 
         const coreSubresult=[];
 
+       
+
       allCRows.forEach((row)=>{
         let name = row.querySelector('.coresub-js').innerHTML;
         let grade = row.querySelector('.coregrade-js').value;
         let type = 'core';
         
+         if(grade ==="Select Grade"|| grade===""){
+            document.querySelector('.prompt').innerHTML='Please select all core subject grades';
+            setTimeout(()=>{
+              document.querySelector('.prompt').innerHTML='';
+            },5500);
+            throw new Error("Missing core grade"); 
+          }
+
       coreSubresult.push({
         type,
         name,
@@ -40,12 +57,32 @@ function values(){
         let grade = row.querySelector('.electivegrade-js').value;
         let type = 'elective';
 
+        if(name ==="Select Course" || grade=="Select Grade" || grade===""){
+          document.querySelector('.prompt').innerHTML='Please select all elective subjects and grades';
+          setTimeout(()=>{
+            document.querySelector('.prompt').innerHTML='';
+          },5500);
+          throw new Error("Missing elective selection"); 
+        }
+
+
       electiveSubresult.push({
         type,
         name,
         grade
       })
       })
+
+      
+
+       if (coreSubresult.length === 0 && electiveSubresult.length === 0) {
+        document.querySelector('.prompt').innerHTML = "No selections made. Please choose your subjects.";
+        setTimeout(()=>{
+          document.querySelector('.prompt').innerHTML ='';
+        },5500)
+        return;
+      }
+
 
       /* console.log(electiveSubresult) */
       //add core to main array
@@ -73,18 +110,8 @@ function values(){
         })
       })
 
-      
 
-      return userResult
-};
-  
-
-
-function processStudentGrades(){
-
-  
-
-  const allResult = values()
+  const allResult = userResult;
 
   const cores = allResult.filter(e=>e.finalType==='core')
   const elective = allResult.filter(e=>e.finalType==='elective')
@@ -94,7 +121,7 @@ function processStudentGrades(){
 
 
   if(uniqueElectiveName.size !== electiveNames.length){
-     document.querySelector('.prompt').innerHTML='cross check your selections';
+     document.querySelector('.prompt').innerHTML='you selected two same courses';
     setTimeout(()=>{
       document.querySelector('.prompt').innerHTML=''
     },5800)
@@ -124,10 +151,48 @@ function processStudentGrades(){
   console.log("this is the final " + finalAggregrate);
   document.querySelector('.prompt').innerHTML=finalAggregrate
 
+  const electiveOne = elective[0];
+  const electivetwo = elective[1];
+  const electivethree = elective[2];
+  const electivefour = elective[3];
 
-  let check =KNUST.filter(course=>finalAggregrate <= course.cutoff_criteria.minimum_aggregate);
-  console.log(KNUST);
-  console.log(check);
+  const lete = [
+   electiveOne,
+  electivetwo,
+  electivethree,
+   electivefour
+  ]
+let i;
+let j;
+  for(i=0;i<lete.length-1;i++){
+    for(j=0;j<lete.length-1;j++){
+        if(lete[j].finalGrade>lete[j + 1].finalGrade){
+            let temp = lete[j];
+            lete[j] = lete[j + 1];
+            lete[j+1] = temp;
+        }
+    }
+  } 
+  console.log(lete);
+
+  const finalBestThree = lete.slice(0,3);
+  console.log(finalBestThree);
+
+ 
+  const studentData = {
+    coreMath:mathGrade,
+    coreScience:scienceGrade,
+    coreEnglish:englishGrade,
+    [finalBestThree[0].finalSub]:finalBestThree[0].finalGrade,
+    [finalBestThree[1].finalSub]:finalBestThree[1].finalGrade,
+    [finalBestThree[2].finalSub]:finalBestThree[2].finalGrade,
+    aggregrate:finalAggregrate
+  };
+
+  console.log(studentData);
+
+  let check =KNUST.filter(course=>finalAggregrate <= course.cutoff_criteria.minimum_aggregate && course.college==="College of Science");
+ console.log(check);
 
   return finalAggregrate
 }
@@ -135,10 +200,10 @@ function processStudentGrades(){
 
 
 document.querySelector('.gradeButton1').addEventListener('click',()=>{
-
- processStudentGrades();
-
+ Getvalues();
 });
+
+
 }
 
 unis();
